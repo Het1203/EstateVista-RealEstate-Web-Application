d5cc8688808f6380fafedafb3932a4eca52e711e
Het Prajapati <het.p4@ahduni.edu.in>
2024-08-20 23:02:39 +0530

    add user and auth routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
+
+const app = express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Database connected');
@@ -10,7 +15,8 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 });
 
-const app = express();
+
+app.use("/", authRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000 ');
