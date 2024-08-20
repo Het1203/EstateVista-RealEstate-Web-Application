2690a020b2998f6601419d7fbb3788996200a76b
Het Prajapati <het.p4@ahduni.edu.in>
2024-08-20 13:15:51 +0530

    Error resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,21 +5,21 @@ import Home from './pages/Home'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <>
+            <header>
+                <h1 className='font-semibold'>
+                    <span className='text-slate-500'>Estate </span>
+                    <span className='text-slate-700'>Vista</span>
+                </h1>
+            </header>
+        </>
+    )
+}
+
+
