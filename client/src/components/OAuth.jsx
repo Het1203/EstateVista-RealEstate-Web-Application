a026d94a4489b02407ccb701b7efe0eef307aea6
Het Prajapati <het.p4@ahduni.edu.in>
2024-08-23 11:23:59 +0530

    Add credentials include option to OAuth endpoint

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -17,6 +17,7 @@ export default function OAuth() {
             const result = await signInWithPopup(auth, provider);
 
             const res = await fetch('http://localhost:3000/google', {
+                credentials: 'include',
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
