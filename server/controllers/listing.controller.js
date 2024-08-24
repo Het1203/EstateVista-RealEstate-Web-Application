2f8b7516bbcda0e3469066e4afd9efe9a015acb1
Het Prajapati <het.p4@ahduni.edu.in>
2024-08-24 22:55:00 +0530

    Add update functionality for user listings

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -32,3 +32,30 @@ export const deleteListing = async (req, res, next) => {
         next(error);
     }
 };
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found'));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(403, 'You are not authorized to delete this listing'));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true
+            }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+
+    }
+
+}
