47e81296ece5f9e95b797f3ee8793e5fc50638d9
Het Prajapati <het.p4@ahduni.edu.in>
2024-08-25 16:01:13 +0530

    Add getListings route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -71,3 +71,59 @@ export const getListing = async (req, res, next) => {
         next(error);
     }
 }
+
+export const getListings = async (req, res, next) => {
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        let offer = req.query.offer;
+        if (offer === undefined || offer === 'false') {
+            offer = { $in: [false, true] };
+        } else if (offer === 'true') {
+            offer = true;
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === 'false') {
+            furnished = { $in: [false, true] };
+        } else if (furnished === 'true') {
+            furnished = true;
+        }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === 'false') {
+            parking = { $in: [false, true] };
+        } else if (parking === 'true') {
+            parking = true;
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === 'all') {
+            type = { $in: ['sale', 'rent'] };
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt';
+
+        const order = req.query.order || 'desc';
+
+        const query = {
+            name: { $regex: searchTerm, $options: 'i' },
+            offer,
+            furnished,
+            parking,
+            type,
+        };
+
+        const listings = await Listing.find(query)
+            .sort({ [sort]: order })
+            .limit(limit)
+            .skip(startIndex);
+
+        return res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+    }
+};
